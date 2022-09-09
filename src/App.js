4b9385990927a8a184ccf1fa18fb1d5dadf8f262
thelarrys <111199849+thelarrys@users.noreply.github.com>
2022-09-09 20:55:25 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -249,7 +249,7 @@ function App() {
             >
               <StyledButton
                 onClick={(e) => {
-                  window.open("/config/roadmap.pdf", "_blank");
+                  window.open("/home/larry/Documents/Website/nft-minting-app/public/config/images/bg.png", "_blank");
                 }}
                 style={{
                   margin: "5px",
